f3bd48cbf7d06ebbe1a9acb2f25ca5da811d3b4e
Paplavskaya <any-lion@mail.ru>
2023-09-24 20:59:59 +0300

    feat - added task_2 in script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ let phone = prompt('Ваш телефон');
 let email = prompt('Ваш email');
 let company = prompt('В какой компании Вы работаете?');
 
-document.write(`Меня зовут ${lastName}. Мне ${age} лет. Я проживаю в 
+document.write(`<h2>Меня зовут ${lastName}. Мне ${age} лет. Я проживаю в 
 городе ${city} и работаю в компании ${company}. Мои контактные данные: 
-${phone}, ${email}`)
+${phone}, ${email}</h2>`)
 
+
+let yearOfBirth = 2023 - age
+
+document.write(`<h2>${lastName} родился в ${yearOfBirth} году</h2>`)
