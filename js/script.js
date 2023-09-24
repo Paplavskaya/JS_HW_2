2ccb5461f8701666d2383ba2d38e2cb2d4abfa39
Paplavskaya <any-lion@mail.ru>
2023-09-24 20:49:40 +0300

    feat - added task_1 in script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let lastName = prompt('Введите ваше имя');
+let age = prompt('Введите ваш возраст');
+let city = prompt('Введите город проживания');
+let phone = prompt('Ваш телефон');
+let email = prompt('Ваш email');
+let company = prompt('В какой компании Вы работаете?');
+
+document.write(`Меня зовут ${lastName}. Мне ${age} лет. Я проживаю в 
+городе ${city} и работаю в компании ${company}. Мои контактные данные: 
+${phone}, ${email}`)
+
