8a37f1cd69195dd674cb8dd5e09635fcc102cfe9
Paplavskaya <any-lion@mail.ru>
2023-09-24 21:13:01 +0300

    feat - added task_3 in script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-let lastName = prompt('Введите ваше имя');
-let age = prompt('Введите ваш возраст');
-let city = prompt('Введите город проживания');
-let phone = prompt('Ваш телефон');
-let email = prompt('Ваш email');
-let company = prompt('В какой компании Вы работаете?');
+// let lastName = prompt('Введите ваше имя');
+// let age = prompt('Введите ваш возраст');
+// let city = prompt('Введите город проживания');
+// let phone = prompt('Ваш телефон');
+// let email = prompt('Ваш email');
+// let company = prompt('В какой компании Вы работаете?');
 
-document.write(`<h2>Меня зовут ${lastName}. Мне ${age} лет. Я проживаю в 
-городе ${city} и работаю в компании ${company}. Мои контактные данные: 
-${phone}, ${email}</h2>`)
+// document.write(`<h2>Меня зовут ${lastName}. Мне ${age} лет. Я проживаю в 
+// городе ${city} и работаю в компании ${company}. Мои контактные данные: 
+// ${phone}, ${email}</h2>`)
 
 
-let yearOfBirth = 2023 - age
+// let yearOfBirth = 2023 - age
 
-document.write(`<h2>${lastName} родился в ${yearOfBirth} году</h2>`)
+// document.write(`<h2>${lastName} родился в ${yearOfBirth} году</h2>`)
+
+
+let str = '123567'
+let сhar1 = +str[0];
+let сhar2 = +str[1];
+let сhar3 = +str[2];
+let сhar4 = +str[3];
+let сhar5 = +str[4];
+let сhar6 = +str[5];
+
+let sum1 = сhar1 + сhar2 + сhar3;
+let sum2 = сhar4 + сhar5 + сhar6;
+sum1 === sum2 ? console.log('да') : console.log('нет');
